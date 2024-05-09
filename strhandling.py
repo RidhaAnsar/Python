77300b6b688b7165f48f20a9830db4e7baf2484f
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-05-09 22:07:44 +0530

    strhandling.py

---
+++ strhandling.py
@@ -0,0 +1,9 @@
+//string handling
+greet="Helloo world"
+print(greet[0:len(greet)])
+print(greet.upper())
+print(greet.lower())
+msg="welcome"
+print(msg.capitalize())
+print(greet.find("world"))
+print(greet.replace("world", "people"))
