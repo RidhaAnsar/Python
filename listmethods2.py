dc96ddf824823d5394a73065a62a1940c509a048
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-05-10 00:03:42 +0530

    listmethods2.py

---
+++ listmethods2.py
@@ -0,0 +1,11 @@
+basket = ['a', 'b', 'c', 'd', 'apple','ball', 'd']
+
+#to get the index of the element
+print(basket.index('d'))
+
+#to check if the elemnt is in the list using 'in'
+print ('a' in basket)
+print('x' in basket)
+
+print(basket.count('d'))
+print(basket.count('cat'))
