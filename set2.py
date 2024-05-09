19932ee9cbd454189e9010e509d260dd42e751df
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-05-10 00:52:44 +0530

    set2.py

---
+++ set2.py
@@ -0,0 +1,14 @@
+set1={1,2,3,4,5}
+set2={1,2,30,40, 50}
+
+#difference
+print(set1.difference(set2))
+
+#union
+print(set1.union(set2))
+
+#intersectionn
+print(set1.intersection(set2))
+
+#disjoint
+print(set1.isdisjoint(set2))
