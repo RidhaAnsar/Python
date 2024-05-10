e7247fc57ea82a8adcb1ee92965c1a6975d51245
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-05-10 12:28:30 +0530

    logicalop2.py

---
+++ logicalop2.py
@@ -0,0 +1,10 @@
+a = input("Enter a: ")
+b = input("Enter b: ")
+c = input("Enter c: ")
+
+if a > b:
+    print("a is greater")
+elif a > c:
+    print("a is greater")
+else:
+    print("b is greater")
