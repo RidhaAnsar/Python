c3f91ed43c2fe01b19c752686eddf04dee30e699
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-05-11 12:26:03 +0530

    listcomprehension.py

---
+++ listcomprehension.py
@@ -0,0 +1,9 @@
+#listcomprehension
+my_list1=[char for char in 'hello']
+my_list2=[num for num in range(1,100)]
+my_list3=[num**2 for num in range(1,100)]
+my_list4=[num**2 for num in range(1,100) if num%2==0]
+print(my_list1)
+print(my_list2)
+print(my_list3)
+print(my_list4)
