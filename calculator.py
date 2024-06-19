dad6f66c7096c38f2a6db6c2a99373e6b232f34f
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-06-19 14:22:31 +0530

    Update calculator.py

--- calculator.py
+++ calculator.py
@@ -1,1 +1,19 @@
-a=input("enter the number")
+n1 = int(input("Enter a number: "))
+n2 = int(input("Enter a number: "))
+x = input("Enter operation to be performed ")
+
+if x == '+':
+    result = n1 + n2
+elif x == '-':
+    result = n1 - n2
+elif x == '*':
+    result = n1 * n2
+elif x == '/':
+    if n2 != 0:
+        result = n1 / n2
+    else:
+        result = "Error: Division by zero"
+else:
+    result = "Error: Invalid operation"
+
+print(result)
