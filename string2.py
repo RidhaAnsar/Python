995527fa15f0376a14255aa113c0e7c10fd2c7ee
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-07-04 19:52:19 +0530

    string2.py

---
+++ string2.py
@@ -0,0 +1,15 @@
+# Define a string
+my_string = "Hello, World!"
+
+# Print the string
+print(my_string)
+
+
+
+# Replace a substring
+replaced_string = my_string.replace("World", "Python")
+print(f"Replaced string: {replaced_string}")
+
+# Slice the string
+sliced_string = my_string[7:12]
+print(f"Sliced string: {sliced_string}")
