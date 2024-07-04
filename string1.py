550451cb940cf158fd0714601a057238e2153c99
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-07-04 19:51:48 +0530

    string1.py

---
+++ string1.py
@@ -0,0 +1,12 @@
+my_string = "Hello, World!"
+
+# Print the string
+print(my_string)
+
+# Get the length of the string
+string_length = len(my_string)
+print(f"Length of the string: {string_length}")
+
+# Convert the string to uppercase
+uppercase_string = my_string.upper()
+print(f"Uppercase: {uppercase_string}")
