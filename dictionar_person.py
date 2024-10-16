4ef18b9779606cd35480ecf20270d24083f1ef13
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-10-16 19:32:47 +0530

    dictionary_person.py

---
+++ dictionar_person.py
@@ -0,0 +1,9 @@
+person={'name':'Ridha', 'Age' : 20, 'Course': 'MCA'}
+print(person)
+
+person['country']='India'   
+print(person)
+person['Age']=22   
+print(person)
+del person['Age']
+print(person)
