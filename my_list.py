315a4df6871df5151c745e83b1048a592f9cd47e
Ridha Ansar <140969987+RidhaAnsar@users.noreply.github.com>
2024-10-16 19:21:36 +0530

    my_list.py

---
+++ my_list.py
@@ -0,0 +1,9 @@
+my_list=['Milu', 'Dudu', 'booboo', 'maama']
+print(my_list[3])
+my_list.append('kiki')
+my_list.remove('maama')
+print(my_list)
+sub_list=my_list[1:4]
+print(sub_list)
+for item in my_list:
+    print(item)
